0a33da7ddaf46d33a8141128585abbece4e6025d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-07-10 13:58:41 +0200

    feat: provide social image and correct base path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://leitfaden-agile-verwaltung.netlify.app/",
+  url: "https://agile-verwaltung.citylab-berlin.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -60,8 +60,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/social-image.png",
       navbar: {
         style: "dark",
         title: "Agil durch den Verwaltungsalltag",
